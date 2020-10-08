9729541cc4fe01a6943a7aacf268ae3bf2c4836b
Levi Halvorson <lhalvorson34@gmail.com>
2020-10-07 18:50:24 -0600

    added js code

---
+++ Develop/README.md
@@ -0,0 +1,40 @@
+
+    #ReadMe Generator
+
+    ## Table of Contents
+
+    - [Description](#description)
+    - [Installastion](#installation)
+    - [Usage](#usage)
+    - [Contributing](#contribution)
+    - [Test](#test)
+    - [Credits](#credits)
+    - [license](#license)
+    - [Questions](#questions)
+
+    ## Description:
+    ![License](https://img.shields.io/badge/license-MIT-blue.svg "License Badge")
+
+        This program takes user input and creates a professional README.md file.
+    ## Installation:
+        None
+    ## Usage:
+        Use this program to generate a readme for your next project!
+    ## Contribution:
+        None
+    ## Test:
+        Use the program and input test text.
+    ## Credits: 
+        Levi Halvorson
+    ## License: 
+        For more information about the license, click on the link below.
+    
+    - [License](https://choosealicense.com/licenses/mit/)
+
+    ## Questions:
+        For questions about this project you can go to my GitHub page at the following link:
+    
+    - [GitHub Profile](https://github.com/Halvosaurus34)
+
+        For additional questions please reach out to me at: lhalvorson34@gmail.com.
+    
--- Develop/index.js
+++ Develop/index.js
@@ -1,15 +1,117 @@
-// array of questions for user
-const questions = [
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
 
-];
+const writeFileAsync = util.promisify(fs.writeFile);
 
-// function to write README file
-function writeToFile(fileName, data) {
+function promptUser() {
+  return inquirer.prompt([
+    {
+      type: "input",
+      message: "What is the name of your project?",
+      name: "title",
+    },
+    {
+      type: "input",
+      message: "Please enter a description for your project.",
+      name: "description",
+    },
+    {
+      type: "input",
+      message: "What are the installation instructions for this project?",
+      name: "install",
+    },
+    {
+      type: "input",
+      message: "Provide some examples on how this project should be used.",
+      name: "usage",
+    },
+    {
+      type: "input",
+      message: "How do you want others to contribute to this project?",
+      name: "contribution",
+    },
+    {
+      type: "input",
+      message: "What are the test instructions?",
+      name: "test",
+    },
+    {
+      type: "checkbox",
+      message: "Please select a license",
+      choices: ["Apache", "MIT", "ISC", "GNU GPLv3"],
+      name: "license",
+    },
+    {
+      type: "input",
+      message: "Who's credit is this work?",
+      name: "credit",
+    },
+    {
+      type: "input",
+      message: "What is your GitHub username?",
+      name: "username",
+    },
+    {
+      type: "input",
+      message: "What is your email address?",
+      name: "email",
+    },
+  ]);
 }
 
-// function to initialize program
-function init() {
+function generateMarkDown(response) {
+  return `
+    #${response.title}
 
+    ## Table of Contents
+
+    - [Description](#description)
+    - [Installastion](#installation)
+    - [Usage](#usage)
+    - [Contributing](#contribution)
+    - [Test](#test)
+    - [Credits](#credits)
+    - [license](#license)
+    - [Questions](#questions)
+
+    ## Description:
+    ![License](https://img.shields.io/badge/license-${response.license}-blue.svg "License Badge")
+
+        ${response.description}
+    ## Installation:
+        ${response.install}
+    ## Usage:
+        ${response.usage}
+    ## Contribution:
+        ${response.contribution}
+    ## Test:
+        ${response.test}
+    ## Credits: 
+        ${response.credit}
+    ## License: 
+        For more information about the license, click on the link below.
+    
+    - [License](https://choosealicense.com/licenses/mit/)
+
+    ## Questions:
+        For questions about this project you can go to my GitHub page at the following link:
+    
+    - [GitHub Profile](https://github.com/${response.username})
+
+        For additional questions please reach out to me at: ${response.email}.
+    `;
+}
+// function to initialize program
+async function init() {
+  try {
+    const response = await promptUser();
+    const readMe = generateMarkDown(response);
+    await writeFileAsync("README.md", readMe);
+    console.log("Success!");
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 // function call to initialize program
